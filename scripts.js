7d5a818716382270d9d3553c9927bdbf67087d3e
YUBBY26 <heyubar26@gmail.com>
2025-01-26 12:11:07 +0200

    Updated User Stories

--- scripts.js
+++ scripts.js
@@ -5,5 +5,17 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  const numDividend = Number(dividend);
+  const numDivider = Number(divider);
+  if (dividend === '' || divider === '') {
+    result.innerText = 'Division not performed. Both values are required in inputs. Try again.';
+  } else if (divider === '0') {
+    result.innerText = 'Division not performed. Invalid number provided. Try again.';
+    console.error('Error: Division by zero attempted.');
+  } else if (isNaN(dividend) || isNaN(divider)) {
+    document.body.innerHTML = "<h1>Error: Something critical went wrong. Please reload the page</h1>";
+    console.error('Error: Non-numeric input detected.');
+} else {
+    result.innerText = Math.trunc(numDividend / numDivider);
+  }
 });
